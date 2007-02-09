3da7ddb69bba2e2590923343c39d96182f8bc36c
jmertic <jmertic@d104be54-2da6-6849-aa9c-4a82b9ac2ed0>
2007-02-09 20:52:50 +0000

    Fixed problems with upgrading to new version.

--- GenPHPInstaller.wxs.php
+++ GenPHPInstaller.wxs.php
@@ -1,10 +1,27 @@
 <?php
+function genGUID() {
+   //e.g. output: 372472a2-d557-4630-bc7d-bae54c934da1
+   //word*2-, word-, (w)ord-, (w)ord-, word*3
+   $guidstr = "";
+   for ($i=1;$i<=16;$i++) {
+      $b = (int)rand(0,0xff);
+      if ($i == 7) { $b &= 0x0f; $b |= 0x40; } // version 4 (random)
+      if ($i == 9) { $b &= 0x3f; $b |= 0x80; } // variant
+      $guidstr .= sprintf("%02s", base_convert($b,10,16));
+      if ($i == 4 || $i == 6 || $i == 8 || $i == 10) { $guidstr .= '-'; }
+   }
+   return strtoupper($guidstr);
+} 
+
 $basefile = $argv[1];
 $version = $argv[2];
 
 $PHPInstallerBaseWXS = DOMDocument::load($basefile);
 $PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Name","PHP $version");
 $PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Version","$version");
+$PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Id",genGUID());
+$PHPInstallerBaseWXS->getElementsByTagName('Package')->item(0)->setAttribute("Description","PHP $version Installer");
 $PHPInstallerBaseWXS->save("PHPInstaller$version.wxs");
+
 exit;
 ?>

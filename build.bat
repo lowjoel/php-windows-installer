df2709f26d9b6e22c07ab8acb110399b17d8ad0c
jmertic <jmertic@d104be54-2da6-6849-aa9c-4a82b9ac2ed0>
2008-04-12 19:24:30 +0000

    Added support for building an installer for the non-thread-safe binaries.

--- build.bat
+++ build.bat
@@ -3,20 +3,25 @@
 set phpver=%1
 set phpver=%phpver:~0,3%
 set phpver=%phpver:.=%
-
 if %phpver%==53 set phpver=52
+
+set msiname="php-%1-win32-installer.msi"
+if "%2"==nts set msiname="php-%1-nts-win32-installer.msi"
+
+set suffix=
+if "%2"==nts set suffix="NTS"
 
 echo Building ExtensionsComponents.wxs
 Files\php.exe GenExtensionsComponents.wxs.php
 
 echo Building PHPInstaller%1.wxs
-Files\php.exe GenPHPInstaller.wxs.php "PHPInstallerBase%phpver%.wxs" "%1"
+Files\php.exe GenPHPInstaller.wxs.php "PHPInstallerBase%phpver%%suffix%.wxs" "%1"
 
 echo Building WebServerConfig%1.wxs
-copy WebServerConfig%phpver%.wxs WebServerConfig%1.wxs
+copy WebServerConfig%phpver%%suffix%.wxs WebServerConfig%1.wxs
 
 echo Compiling UI....
-Wix\candle.exe PHPInstallerCommon.wxs
+Wix\candle.exe -out PHPInstallerCommon.wixobj PHPInstallerCommon%suffix%.wxs
 
 echo Building UI....
 Wix\lit.exe -out PHPInstallerCommon.wixlib PHPInstallerCommon.wixobj 
@@ -25,6 +30,5 @@ echo Compiling Installer....
 Wix\candle.exe ExtensionsComponents.wxs ExtensionsFeaturesBuild.wxs WebServerConfig%1.wxs PHPInstaller%1.wxs 
 
 echo Linking Installer....
-Wix\light.exe -out "php-%1-win32-installer.msi" ExtensionsComponents.wixobj ExtensionsFeaturesBuild.wixobj WebServerConfig%1.wixobj PHPInstaller%1.wixobj PHPInstallerCommon.wixlib -loc WixUI_en-us.wxl
-
+Wix\light.exe -out "%msiname%" ExtensionsComponents.wixobj ExtensionsFeaturesBuild.wixobj WebServerConfig%1.wixobj PHPInstaller%1.wixobj PHPInstallerCommon.wixlib -loc WixUI_en-us.wxl
 set phpver=

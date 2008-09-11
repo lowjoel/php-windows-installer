f0b4f70550cc597a96aada19ab1d808922ca1cd9
jmertic <jmertic@d104be54-2da6-6849-aa9c-4a82b9ac2ed0>
2008-09-11 18:54:57 +0000

    Enabled building of installer for VC9 binaries

--- GenPHPInstaller.wxs.php
+++ GenPHPInstaller.wxs.php
@@ -3,15 +3,43 @@ require_once "GenFunctions.php";
 
 $basefile = $argv[1];
 $version = $argv[2];
+$includemsm = $argv[3];
 
 $PHPInstallerBaseWXS = new DOMDocument;
 $PHPInstallerBaseWXS->preserveWhiteSpace = false;
 $PHPInstallerBaseWXS->load($basefile);
 $PHPInstallerBaseWXS->formatOutput = true;
+
+// Update the Package info to be specific for this build
 $PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Name","PHP $version");
 $PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Version","$version");
 $PHPInstallerBaseWXS->getElementsByTagName('Product')->item(0)->setAttribute("Id",genGUID());
 $PHPInstallerBaseWXS->getElementsByTagName('Package')->item(0)->setAttribute("Description","PHP $version Installer");
+
+// Add in the VC9 MSM if we are building that installer
+if ( !empty($includemsm) ) {
+	$TargetDir = $PHPInstallerBaseWXS->getElementsByTagName('Directory')->item(0);
+	$Merge = $PHPInstallerBaseWXS->createElement('Merge');
+	$Merge = $TargetDir->appendChild($Merge);
+	$Merge->setAttribute('Id','VCRedist');
+	$Merge->setAttribute('SourceFile','Microsoft_VC90_CRT_x86.msm');
+	$Merge->setAttribute('DiskId','1');
+	$Merge->setAttribute('Language','0');
+	
+	$MainFeature = $PHPInstallerBaseWXS->getElementsByTagName('Feature')->item(0);
+	$Feature = $PHPInstallerBaseWXS->createElement('Feature');
+	$Feature = $MainFeature->appendChild($Feature);
+	$Feature->setAttribute('Id','VCRedist');
+	$Feature->setAttribute('Title','Visual C++ 9.0 Runtime');
+	$Feature->setAttribute('AllowAdvertise','no');
+	$Feature->setAttribute('Display','hidden');
+	$Feature->setAttribute('Level','1');
+	
+	$MergeRef = $PHPInstallerBaseWXS->createElement('MergeRef');
+	$MergeRef = $Feature->appendChild($MergeRef);
+	$MergeRef->setAttribute('Id','VCRedist');
+}
+
 $PHPInstallerBaseWXS->save("PHPInstaller$version.wxs");
 
 // remove extension info from php.ini-recommended
--- build.bat
+++ build.bat
@@ -7,19 +7,31 @@ set phpver=%phpver:.=%
 echo Building ExtensionsFeatures.wxs
 copy ExtensionsFeatures%phpver%.wxs ExtensionsFeatures.wxs
 
-set msiname="php-%1-win32-installer.msi"
 set suffix=
+set extrants=
+set extrabuildtype=
+set includevc9msm=
 
 if (%2)==() goto build
-if %2==nts set msiname="php-%1-nts-win32-installer.msi"
+if %2==nts set extrants="nts-"
 if %2==nts set suffix="NTS"
+if %2==vc9 set extrabuildtype="vc9-"
+if %2==vc9 set includevc9msm="x86"
+
+if (%3)==() goto build
+if %3==nts set extrants="nts-"
+if %3==nts set suffix="NTS"
+if %3==vc9 set extrabuildtype="vc9-"
+if %3==vc9 set includevc9msm="x86"
 
 :build
+set msiname="php-%1-%extrants%win32-%extrabuildtype%installer.msi"
+
 echo Building ExtensionsComponents.wxs
 Files\php.exe GenExtensionsComponents.wxs.php "%phpver%"
 
 echo Building PHPInstaller%1.wxs
-Files\php.exe GenPHPInstaller.wxs.php "PHPInstallerBase%phpver%%suffix%.wxs" "%1"
+Files\php.exe GenPHPInstaller.wxs.php "PHPInstallerBase%phpver%%suffix%.wxs" "%1" "%includevc9msm%"
 
 echo Building WebServerConfig%1.wxs
 copy WebServerConfig%phpver%%suffix%.wxs WebServerConfig%1.wxs
